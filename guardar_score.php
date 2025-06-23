ce06888f12382871bf6eff0aeca323dfd5b190b8
Gianmarco Rivera <gianrc91@gmail.com>
2025-06-23 00:52:55 -0500

    Actualización de archivos PHP y CORS

--- guardar_score.php
+++ guardar_score.php
@@ -1,9 +1,11 @@
 <?php
-// Encabezados CORS
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Methods: POST, OPTIONS');
-header('Content-Type: application/json');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: Content-Type');
+    header('Access-Control-Allow-Methods: POST, OPTIONS');
+    http_response_code(200);
+    exit;
+}
 
 // Manejar preflight CORS
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
--- obtener_scores.php
+++ obtener_scores.php
@@ -1,5 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: POST');
 header('Content-Type: application/json');
 
 // Opcionalmente manejar OPTIONS también
